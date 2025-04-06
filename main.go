bc784b90e42f4f5e8e0ae7e2b9998283f4b3e840
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-06 17:25:28 -0300

    go types

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	// := -> declaração e atribuição com inferência de tipo
+	a := "cleiton"
+	b := true
+	c := 32
+	d := 1.1
+	e := ` lalal
+	teste `
+
+	fmt.Println("aqui o nome:", a)
+	fmt.Println("b:", b)
+	fmt.Println("c:", c)
+	fmt.Println("d:", d)
+	fmt.Println("e:", e)
 }
