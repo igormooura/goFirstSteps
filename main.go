b9792f073699797aabb1d5918fa26c091e82b0a5
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-06 17:46:28 -0300

    for loop and arrays com make e append

--- main.go
+++ main.go
@@ -28,4 +28,38 @@ func main() {
 	result2 := math.SquareX(2000000000)
 	fmt.Println(result2)
 
+	// array com make
+
+	var tamanho int
+	fmt.Print("Digite o tamanho do array: ")
+	fmt.Scanln(&tamanho)
+
+	arr := make([]int, tamanho) // make já pré determina
+
+	fmt.Println("Digite os números do array:")
+	for i := 0; i < tamanho; i++ {
+		fmt.Printf("Número %d: ", i+1)
+		fmt.Scanln(&arr[i])
+	}
+
+	fmt.Println("\nArray digitado:", arr)
+
+	// array com append
+
+	var tamanho2 int
+	fmt.Print("Digite o tamanho do array: ")
+	fmt.Scanln(&tamanho2)
+
+	var arr2 []int // slice vazio
+
+	fmt.Println("Digite os números do array:")
+	for i := 0; i < tamanho2; i++ {
+		var valor int
+		fmt.Printf("Número %d: ", i+1)
+		fmt.Scanln(&valor)
+		arr2 = append(arr2, valor)
+	}
+
+	fmt.Println("\nArray digitado:", arr2)
+
 }
